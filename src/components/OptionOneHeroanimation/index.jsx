426ad5b31003a129fc53790a0666ebeee031ca46
Khusbu853 <guptakhusbu244@gmail.com>
2024-03-15 15:24:34 +0530

    slider added

--- src/components/OptionOneHeroanimation/index.jsx
+++ src/components/OptionOneHeroanimation/index.jsx
@@ -1,19 +1,40 @@
 import React from "react";
-import { Button, Text, Img } from "./..";
+import { Button, Text } from "./..";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 export default function OptionOneHeroanimation({
   interiorsmade = "Interiors Made easy",
   indulgein = "Indulge in Uncompromising Quality and Timeless Elegance for your Home Projects",
   chatWithUs = "Chat with Us",
+  images = [
+    { src: "images/img_screenshot_2023.png", alt: "screenshot2023" },
+    { src: "images/img_home2.svg", alt: "home2" },
+    { src: "images/img_home3.svg", alt: "home3" },
+    // Add more images as needed
+  ],
   ...props
 }) {
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 2000,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 3000, // Change this value to adjust autoplay speed in milliseconds
+  };
+
   return (
     <div {...props}>
-      <Img
-        src="images/img_screenshot_2023.png"
-        alt="screenshot2023"
-        className="justify-center h-[734px] w-full left-0 bottom-0 right-0 top-0 m-auto object-cover absolute"
-      />
+      <Slider {...settings}>
+        {images.map((image, index) => (
+          <div key={index}>
+            <img src={image.src} alt={image.alt} className="w-full" />
+          </div>
+        ))}
+      </Slider>
       <div className="flex w-full h-full pl-20 pr-14 left-0 bottom-0 right-0 top-0 py-20 m-auto md:p-5 bg-gradient absolute">
         <div className="flex flex-col items-start w-[47%] gap-[49px] my-[92px]">
           <div className="flex flex-col self-stretch gap-6">
@@ -32,4 +53,5 @@ export default function OptionOneHeroanimation({
     </div>
   );
 }
+
 
